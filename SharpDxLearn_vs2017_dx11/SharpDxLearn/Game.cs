77a5e925e6da34981b26c3bb039a195b5c3eb029
wth <690879430@qq.com>
2019-06-24 12:14:59 +0800

    添加测试工程

---
+++ SharpDxLearn_vs2017_dx11/SharpDxLearn/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.Windows;
+using System.Drawing;
+
+public class Game : IDisposable
+{
+    RenderForm m_renderForm;
+
+    int m_width = 1280;
+    int m_height = 720;
+
+    public Game()
+    {
+        m_renderForm = new RenderForm("SharpDxLearn");
+        m_renderForm.ClientSize = new Size(m_width, m_height);
+        m_renderForm.AllowUserResizing = false;
+    }
+
+    public void Run()
+    {
+        RenderLoop.Run(m_renderForm, RenderCallback);
+    }
+
+    void RenderCallback()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        m_renderForm.Dispose();
+    }
+}
---
+++ SharpDxLearn_vs2017_dx11/SharpDxLearn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("test");
+        //Console.ReadLine();
+        using(Game game = new Game())
+        {
+            game.Run();
+        }
+    }
+}
+
